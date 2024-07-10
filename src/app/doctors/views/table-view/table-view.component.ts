fb84936a1449b1dcdd910a8fe6acf8a6cb10bad4
NikaKlokava <veronika.kazakevich1997@gmail.com>
2024-07-10 11:07:27 +0300

    fix: fix comments

--- src/app/doctors/views/table-view/table-view.component.ts
+++ src/app/doctors/views/table-view/table-view.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Doctor } from 'src/app/shared/components/doctor-card/doctor.model';
-import { TABLE_HEADERS } from 'src/app/shared/constants';
-import { DoctorsService } from '../../services/doctors.service';
 import { AuthService } from 'src/app/core/auth.service';
 import { FirebaseService } from 'src/app/shared/services/firebase.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'hp-table-view',
@@ -12,20 +11,19 @@ import { FirebaseService } from 'src/app/shared/services/firebase.service';
 })
 export class TableViewComponent {
   @Input() doctors!: Doctor[];
-  isAdmin!: boolean;
+  isAdmin!: Observable<boolean>;
 
   constructor(
     private authService: AuthService,
     private firebaseService: FirebaseService
   ) {
-    this.authService.isAdmin.subscribe((data) => (this.isAdmin = data));
+    this.isAdmin = this.authService.isAdmin;
   }
 
   onTopDoctorChange(doctor: Doctor) {
     this.firebaseService.changeTopDoctors(doctor);
   }
 
-  tableHeaders = TABLE_HEADERS;
   trackByFn(_index: number, item: Doctor): string {
     return item.id;
   }
